8223bce03d0a17fc085de94535842703cff0598c
Rohan Aggarwal <aggarwal.rohan17@gmail.com>
2020-02-25 18:51:36 +0400

    docker volume and docker compose example

---
+++ redis-image/simpleweb/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+
+const app = express();
+
+app.get('/',(req,res) => {
+        res.send('Hello there')
+})
+
+app.listen('8080',() => {
+    console.log('Listening on port 8080')
+});
